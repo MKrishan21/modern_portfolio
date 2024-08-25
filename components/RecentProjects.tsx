461e39af2b7f81f72c4a4d78647daf1007f2a6ad
MKrishan21 <kgmishra21@gmail.com>
2024-08-25 21:39:22 +0530

    updated

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -42,7 +42,7 @@ const RecentProjects = () => {
                 </div>
                 <div className="flex justify-center items-center">
                   <p className="flex lg:text-xl md:text-xs text-sm text-purple">
-                    Check Live Site
+                    Check Live Site Now
                   </p>
                   <FaLocationArrow className="ms-3" color="#CBACF9" />
                 </div>
